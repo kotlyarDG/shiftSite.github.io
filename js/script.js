1bb01d5e457e47150d67b202c9465717ff079f91
kotlyarDG <kotlyar.d.g@gmail.com>
2020-08-14 19:40:31 +0300

    main-head

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 // Проверка, загрузилась ли страница
-$(document).ready(function(){
+$(document).ready(function () {
+	$('.header__search-btn').click(function (event) {
+		$('.header__search-btn').toggleClass('_disable')
+		$('.header__search').toggleClass('_active')
+	});
+
+
+	$(document).mouseup(function (event) {
+		if ($('form.header__search').hasClass('_active')) {
+			if ($(".header__search").has(event.target).length === 0) {
+				$('.header__search-btn').toggleClass('_disable')
+				$('.header__search').toggleClass('_active')
+			}
+		}
+	});
 
 });
