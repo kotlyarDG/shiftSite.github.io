eb18a32430fb9190608aa6be6e124dd3d9b35173
kotlyarDG <kotlyar.d.g@gmail.com>
2020-08-16 18:07:33 +0300

    answer + phone

--- js/script.js
+++ js/script.js
@@ -15,4 +15,29 @@ $(document).ready(function () {
 		}
 	});
 
+	$('.item-answer__title').click(function (event) {
+		$('.item-answer__title').not($(this)).removeClass('_active');
+		$('.item-answer__text').not($(this).next()).slideUp(300);
+
+		$(this).toggleClass('_active').next().slideToggle(300);
+	});
+	$('.item-info').hide();
+	var cat = $('.cases__item._active').data('filter');
+	$('.item-info.f_' + cat).show();
+
+	$('.cases__item').click(function (event) {
+		var cat = $(this).data('filter');
+
+		// Присвоение ссылке класса active-filter
+		$('.cases__item._active').removeClass('_active')
+		$(this).addClass('_active')
+
+		// При категории 1 - показывает все элементы с классом filter__item
+		// В ином случае показывает элементы с классом f_ + номер категории
+
+		$('.item-info').hide();
+		$('.item-info.f_' + cat).fadeToggle();
+	});
+
+
 });
