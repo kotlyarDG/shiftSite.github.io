912522e44383c8d87b28adf4b7ee716b648c9a4f
kotlyarDG <kotlyar.d.g@gmail.com>
2020-08-16 21:42:36 +0300

    v1

--- js/script.js
+++ js/script.js
@@ -61,4 +61,148 @@ $(document).ready(function () {
 			$('.rates__items--year').fadeToggle(300);
 		}
 	});
+
+	$(document).on('click', '.class-link', function () {
+		var linkID = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(linkID).offset().top
+		}, 'slow');
+	});
+
+	$(document).on('click', '.burger-link', function () {
+		var linkID = $(this).attr('href');
+		$('.header__burger,.burger__menu').toggleClass('_active');
+		$('body').toggleClass('_lock');
+		$('html, body').animate({
+			scrollTop: $(linkID).offset().top
+		}, 'slow');
+	});
+
+	const popupLinks = document.querySelectorAll('.popup-link');
+	const body = document.querySelector('body');
+	const lockPadding = document.querySelectorAll('.lock-padding');
+	const video = document.querySelector('.video-youtube');
+
+	let unlock = true;
+
+	// timeout - время анимации в css
+	const timeout = 880;
+
+	if (popupLinks.length > 0) {
+		for (let index = 0; index < popupLinks.length; index++) {
+			const popupLink = popupLinks[index];
+			popupLink.addEventListener("click", function (e) {
+				const popupName = popupLink.getAttribute('href').replace('#', '');
+				const currentPopup = document.getElementById(popupName);
+				popupOpen(currentPopup);
+				e.preventDefault();
+			});
+		}
+	}
+
+	const popupCloseIcon = document.querySelectorAll('.close-popup');
+	if (popupCloseIcon.length > 0) {
+		for (let index = 0; index < popupCloseIcon.length; index++) {
+			const el = popupCloseIcon[index];
+			el.addEventListener('click', function (e) {
+				popupClose(el.closest('.popup'));
+				e.preventDefault();
+			});
+		}
+	}
+
+	function popupOpen(currentPopup) {
+		if (currentPopup && unlock) {
+			const popupActive = document.querySelector('.popup.open');
+			if (popupActive) {
+				popupClose(popupActive, false);
+			} else {
+				bodyLock();
+			}
+			currentPopup.classList.add('open');
+			video.setAttribute('src', 'https://www.youtube.com/embed/9lO06Zxhu88');
+			currentPopup.addEventListener('click', function (e) {
+				if (!e.target.closest('.popup__content')) {
+					popupClose(e.target.closest('.popup'));
+				}
+			});
+		}
+	}
+
+	function popupClose(popupActive, doUnlock = true) {
+		if (unlock) {
+			popupActive.classList.remove('open');
+			video.setAttribute('src', '');
+			if (doUnlock) {
+				bodyUnlock();
+			}
+		}
+	}
+
+
+	function bodyLock() {
+		// const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+		// if (lockPadding.length > 0) {
+		// 	for (let index = 0; index < lockPadding.length; index++) {
+		// 		const el = lockPadding[index];
+		// 		el.style.paddingRight = lockPaddingValue;
+		// 	}
+		// }
+		// body.style.paddingRight = lockPaddingValue;
+		body.classList.add('_lock');
+
+		unlock = false;
+		setTimeout(function () {
+			unlock = true;
+		}, timeout);
+	}
+
+	function bodyUnlock() {
+		setTimeout(function () {
+			for (let index = 0; index < lockPadding.length; index++) {
+				const el = lockPadding[index];
+				el.style.paddingRight = '0px';
+			}
+			body.style.paddingRight = '0px';
+			body.classList.remove('_lock');
+		}, timeout);
+
+		unlock = false;
+		setTimeout(function () {
+			unlock = true;
+		}, timeout);
+	}
+
+	document.addEventListener('keydown', function (e) {
+		if (e.which === 27) {
+			const popupActive = document.querySelector('.popup.open');
+			popupClose(popupActive);
+		}
+	});
+
+	(function () {
+		// проверяем поддержку
+		if (!Element.prototype.closest) {
+			// реализуем
+			Element.prototype.closest = function (css) {
+				var node = this;
+				while (node) {
+					if (node.matches(css)) return node;
+					else node = node.parentElement;
+				}
+				return null;
+			};
+		}
+	})();
+	(function () {
+		// проверяем поддержку
+		if (!Element.prototype.matches) {
+			// определяем свойства
+			Element.prototype.matches = Element.prototype.matchesSelector ||
+				Element.prototype.webkitMatchesSelector ||
+				Element.prototype.mozMatchesSelector ||
+				Element.prototype.msMatchesSelector;
+		}
+	})();
 });
